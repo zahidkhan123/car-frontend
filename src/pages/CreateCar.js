4b2111e370fcd694a268baa3ccb1bed3d2b7d2f9
zahid <zahidkhandev@gmail.com>
2024-10-04 22:38:38 +0500

    useNavigate removed

--- src/pages/CreateCar.js
+++ src/pages/CreateCar.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
